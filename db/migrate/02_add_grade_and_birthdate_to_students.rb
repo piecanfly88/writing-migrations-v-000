85e5376c7c6f70e2aef9251a6a1c4f23efae6fe0
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-17 23:26:09 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Base[5.1]
-def change
-  add_column :students, :grade, :Integer 
-  add_column :students, :birthdate, :string  
-end
+  def change
+    add_column :students, :grade, :Integer 
+    add_column :students, :birthdate, :string  
+  end
 end
